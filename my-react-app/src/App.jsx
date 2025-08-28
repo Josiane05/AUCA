2bba8661db9f81818f329bb8070c080ba5b63e07
Josiane05 <josianemushimiyimana05@gmail.com>
2025-08-28 15:58:30 +0100

    Todo List

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,3 +1,30 @@
+import "./index.css"
+
 export default function App() {
-  return "Hi"
+  return (
+    <div>
+         <form className="new-item-form">
+    <div className="form-row">
+      <label htmlFor="item">New Item</label>
+      <input type="text" id="item"/>
+      </div>
+      <botton className="btn">add</botton>
+  </form> 
+  <h1 className="header">Todo List</h1>.
+  <ul className="list">
+    <li>
+      <label>
+        <input type="checkbox"/>
+        item 1
+      </label>
+      <botton className="btn btn-danger">Delete</botton>
+    </li>
+    </ul>
+  <label>
+    <input type="checkbox"/>
+    item 2
+  </label>
+  <botton className="btn btn-danger">Delete</botton>
+</div>
+  )  
 }
